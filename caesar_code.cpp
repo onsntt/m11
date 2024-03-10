a49d45089485f87ae67721d7a6d673aa19e20cea
onsntt <onsnt@mail.ru>
2024-03-10 13:07:02 +0500

    caesar_code is ready

---
+++ caesar_code.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+char code(char c, int k)
+{
+    if (c >= 'a' && c <= 'z')
+    {
+        c = 'a' + ((c - 'a' + k) % 26);
+    }
+    else if (c >= 'A' && c <= 'Z')
+    {
+        c = 'A' + ((c - 'A' + k) % 26);
+    }
+    else if (c >= '0' && c <= '9')
+    {
+        c = '0' + ((c - '0' + k) % 10);
+    }
+    else if (c == ' ')
+    {
+        c = ' ';
+    }
+    //std::cout << c << "\n";
+    return c;
+}
+int main()
+{
+    std::cout << "Enter your string!\n";
+    std::string str;
+    getline(std::cin, str);
+    std::cout << "Enter your Key !\n";
+    int key;
+    std::cin >> key;
+    std::string result = "";
+    for (int i = 0; i < str.length(); i++)
+    {
+        result += code(str[i], key);
+    }
+    std::cout << result;
+}
